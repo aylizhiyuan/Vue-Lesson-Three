2c1ed24d701ddedb24b45a0784a9f7bab6c821cf
lizhiyuan <148334733@qq.com>
2018-11-28 17:50:23 +0800

    有个小bug.....

--- admin/src/components/common/ArticleList.vue
+++ admin/src/components/common/ArticleList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <ul class="list">
-        <li class="article"  :class="{active: activeIndex === index, published: isPublished === 1}" v-for="{title,createTime,isPublished,isChosen},index in articleList" @click="select(index)">
+        <li class="article"  :class="{active: activeIndex === index, published: isPublished === 1}" v-for="{title,createTime,isPublished,isChosen},index in articleList" @click="select(index)" v-if="isChosen">
           <header>{{title}}</header>
           <p>{{createTime}}</p>
         </li>
@@ -72,6 +72,40 @@
         }).catch(err=>{
           console.log(err);
         })
+      },
+      updateListByTags(chosenTags){
+        //如果没有选中任何标签，直接显示所有的文章.
+        if(chosenTags.length === 0){
+          for(let article of this.articleList){
+            article.isChosen = true
+          }
+        }else{
+          //如果选中了某些标签的话
+          for(let article of this.articleList){
+            let flag = false
+            for(let tag of chosenTags){
+              if(article.tags.indexOf(tag) !== -1){
+                flag = true
+              }
+            }
+            //标签对应的文章的isChosen设置为true,否则的话设置为false
+            if(flag){
+              article.isChosen = true
+            }else{
+              article.isChosen = false
+            }
+          }
+          //这里，意思是将最后一篇文章选中作为当前正在编辑的文章
+          for(let [index,article] of this.articleList.entries()){
+            if(article.isChosen){
+              this.activeIndex = index
+              console.log(this.activeIndex);
+              this.SET_CURRENT_ARTICLE(this.articleList[this.activeIndex])
+              //发现第一篇文章后就跳出了，所有就不用管第二篇文章了
+              break
+            }
+          }
+        }
       },
       select(index){
         this.activeIndex = index
